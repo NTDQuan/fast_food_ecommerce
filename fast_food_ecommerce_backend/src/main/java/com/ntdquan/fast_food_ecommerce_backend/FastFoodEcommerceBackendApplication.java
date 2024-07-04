8a1b5afe3f6bdda85dc9b2e0edb773f7caa9a06b
Dinh Quan <ntdquan2003@gmail.com>
2024-07-04 14:06:29 +0700

    Add code base

---
+++ fast_food_ecommerce_backend/src/main/java/com/ntdquan/fast_food_ecommerce_backend/FastFoodEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ntdquan.fast_food_ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ fast_food_ecommerce_backend/src/test/java/com/ntdquan/fast_food_ecommerce_backend/FastFoodEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntdquan.fast_food_ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
